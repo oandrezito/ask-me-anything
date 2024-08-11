5983c6e56db25edaa28ff8084d689352181a7b46
Andre Fernando Lara <andrefernandolara@hotmail.com>
2024-08-11 18:08:37 -0300

    add backend\frontend

---
+++ frontend/src/http/remove-message-reaction.ts
@@ -0,0 +1,10 @@
+interface RemoveMessageReaction {
+  roomId: string
+  messageId: string
+}
+
+export async function removeMessageReaction({ messageId, roomId }: RemoveMessageReaction) {
+   await fetch (`${import.meta.env.VITE_APP_API_URL}/rooms/${roomId}/messages/${messageId}/react`, {
+    method: 'DELETE',
+  })
+}
